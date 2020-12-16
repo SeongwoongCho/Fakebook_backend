9b542f62ab718b9fbae5912b0a017c4d5ab99320
minseongg <asdjke@kaist.ac.kr>
2020-12-16 05:40:45 +0000

    Optimize and Ignore .h5 files

--- app.py
+++ app.py
@@ -5,12 +5,18 @@ import cv2
 import warnings
 import numpy as np
 import imageio as io
+import secrets
+import pymysql
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from google.cloud import storage
+from config import Config
 
 sys.path.append('./fewshot-face-translation-GAN')
 warnings.filterwarnings('ignore')
 
+from pygifsicle import optimize
 from models import FaceTranslationGANInferenceModel
 from face_toolbox_keras.models.verifier.face_verifier import FaceVerifier
 from face_toolbox_keras.models.parser import face_parser
@@ -20,7 +26,25 @@ from utils import utils
 
 
 app = Flask(__name__)
+CORS(app)
+config = Config()
 models = None
+db = pymysql.connect(
+    host=       config.DB_HOST,
+    port=       config.DB_PORT,
+    user=       config.DB_USER,
+    passwd=     config.DB_PASSWD,
+    db=         config.DB_SCHEMA,
+    charset=    config.DB_CHARSET
+)
+
+
+def exec_sql(db, sql):
+    with db.cursor() as cursor:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        db.commit()
+    return result
 
 
 def load(filepath):
@@ -75,25 +99,55 @@ def inference(models, images, template):
     return result
 
 
+def upload_blob(bucket_name, src, dest):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(dest)
+    blob.upload_from_filename(src)
+    print ("File {} uploaded to {}.".format(src, dest))
+    pass
+
+
 @app.route('/generate', methods=['POST'])
 def route_generate():
     args = request.json
+    name, img_bytes, template = args['name'], args['img_bytes'], args['template']
+    print(name, img_bytes, template)
 
+    # Generate a token corresponding to the image
+    token = secrets.token_urlsafe(16)
+
+    # Add information to Database
+    sql = f"INSERT INTO images(token, name, status) VALUES ('{token}', '{name}', 'converting')"
+    exec_sql(db, sql)
+
+    # Image preprocessing
     img_data = base64.b64decode(args['img_bytes'])
     img_np = np.frombuffer(img_data, dtype=np.uint8)
-
     img = cv2.imdecode(img_np, flags=1)[...,::-1]
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_list = [img]
-    template_path = './datas/gif/minseong-sexy-body.gif'
+    
+    template_path = './assets/gif/{}.gif'.format(template)
 
+    # Make inference
     result = inference(models, images=img_list, template=template_path)
 
-    with io.get_writer('./datas/result0.gif', mode='I', duration=0.1) as writer:
+    # Save to local file system
+    local_path = os.path.join('temp/', token + '.gif')
+    with io.get_writer(local_path, mode='I', duration=0.1) as writer:
         for image in result:
             writer.append_data(image)
+    optimize(local_path)
+    # Upload to Google Cloud Storage
+    remote_path = os.path.join('posts/', token + '.gif')
+    upload_blob("fakebook-posts", local_path, remote_path)
 
-    return jsonify({ 'len': len(result) })
+    # Add information to Database
+    sql = f"UPDATE images SET status='converted' WHERE token='{token}'"
+    exec_sql(db, sql)
+
+    return jsonify({ 'remote_path': remote_path })
 
 
 if __name__ == '__main__':
